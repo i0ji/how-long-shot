b33ef7a146a627474792b6a118c72550e428f84d
i0ji <ioji@inbox.ru>
2026-03-11 15:45:29 +0300

    feat(full product): remove sass; set up env \& structure
    Version: 0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ function App() {
   return (
     <>
       <h1 className="bg-yellow-600">hello</h1>
+      <p className="fontBold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quibusdam esse et voluptatem
+        optio fugiat, exercitationem qui dolores iure ipsum! Quos similique maxime cum magnam
+        doloremque veniam voluptatibus, ut voluptatum.
+      </p>
+      <p className="fontRegular">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quibusdam esse et voluptatem
+        optio fugiat, exercitationem qui dolores iure ipsum! Quos similique maxime cum magnam
+        doloremque veniam voluptatibus, ut voluptatum.
+      </p>
     </>
   );
 }
